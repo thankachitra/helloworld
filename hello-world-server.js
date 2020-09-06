c63054af67ab5e9b909de0eb5a5bbe450c2274a0
thankachitra <abipranavi@thankactrasimac.lan>
2020-09-05 21:14:59 -0700

    changed port

--- hello-world-server.js
+++ hello-world-server.js
@@ -1,6 +1,8 @@
 var http = require('http');
+const PORT = 8080;
+const HOST = '0.0.0.0';
 http.createServer(function handler(req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('welcome to dev ops world\n');
-}).listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
+    res.end('ALL GOOD welcome to dev ops world\n');
+}).listen(PORT, HOST);
+console.log('Server running at http://${HOST}:${PORT/');
